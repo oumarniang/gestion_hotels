7935f3a4f3086215590e710124047be7a18f71a9
flabastie <flabastie@hotmail.com>
2021-10-14 15:53:38 +0200

    Nettoyage code

--- mon_app/project/main.py
+++ mon_app/project/main.py
@@ -38,8 +38,6 @@ def reservations():
     for item in result_columns:
         column_name = list(item)[1]
         columns_names.append(column_name)
-        # print(column_name)
-    # print(columns_names)
 
     # data query
     cur2 = conn.cursor()
@@ -56,32 +54,16 @@ def reservations():
         result_data_list.append(item_dict)
 
     # create result dictionnary
-    print(result_data_list)
-
-
-    # cur = conn.cursor()
-    # # Return first result of query
-    # cur.execute('SELECT id, lead_time, arrival_date_year, arrival_date_week_number, stays_in_week_nights, \
-    # adults, children, days_in_waiting_list, adr \
-    # FROM hotel WHERE arrival_date_year="2015" LIMIT 20')
-    # result = cur.fetchall()
-
+    # print(result_data_list)
     conn.close()
-
-    # for item in result:
-    #     print(item)
 
     return render_template('reservations.html', clients_test=result_data_list )
  
 
 ## Page de prédictions
-# @main.route('/predictions', methods = ['POST', 'GET'])
 @main.route('/predictions/<data_client>', methods = ['POST', 'GET'])
 @login_required
 def predictions(data_client):
-
-    # print(data_client)
-    # df = pd.DataFrame.from_dict(data_client)
     
     sample = ast.literal_eval(data_client)
     del sample['id']
@@ -92,42 +74,10 @@ def predictions(data_client):
     model_pred = pickle.load(open('project/data/model.pkl','rb'))
     predict_result = model_pred.predict(df)
 
-    # conn = sql.connect('project/data/hotel.db')
-
-    # # cur1 = conn.cursor()
-    # # cur1.execute("SELECT * FROM PRAGMA_TABLE_INFO('hotel')")
-    # # result_columns = cur1.fetchall()
-    # # print(result_columns)
-
-    # cur = conn.cursor()
-    # # Return first result of query
-    # cur.execute('SELECT * FROM hotel WHERE id=?', [id_client])
-    # result = cur.fetchall()
-
-    # # modify tuple to list
-    # sample = list(result[0])
-    # # remove 2 first elements from sample list
-    # del sample[0:2]
-
-    # conn.close()
-
-    # print(type(sample))
-    # print(sample)
-    # # for item in result:
-    # #     print(item)
-
-    # # X_test = pd.DataFrame(result, columns=result_columns)
-    # print('=========')
-    # # print(X_test)
-
-    # # model_pred = pickle.load(open('project/data/model.pkl','rb'))
-    # # print(model_pred.predict(result))
-
-
     return render_template('predictions.html', data_client=data_client, predict_result=predict_result[0])
 
 
-## Page d'Accuiel
+## Page Accueil
 @main.route('/home')
 def home():
     '''
